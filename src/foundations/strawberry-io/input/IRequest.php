6ba9d55311818d3eccf345bbef59c76a8b8d6e9e
elderguardian <katzenmaedchen@tutanota.com>
2023-05-22 21:45:55 +0200

    fixed submodule bug

---
+++ src/foundations/strawberry-io/input/IRequest.php
@@ -0,0 +1,12 @@
+<?php
+
+interface IRequest
+{
+    function fetchRequestType();
+
+    function fetchData();
+
+    function fetch(string $paramName, $filter = null);
+
+    function fetchOrFail(string $paramName, $filter = null);
+}
