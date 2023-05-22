72d9d279ef759aeacf4aa838530c024e307e8d63
elderguardian <katzenmaedchen@tutanota.com>
2023-05-22 21:35:16 +0200

    added route for image view

--- routes.php
+++ routes.php
@@ -3,6 +3,7 @@
 $routes = [
 
     '/upload' => [UploadController::class, 'index', [TokenMiddleware::class]],
+    '/' => [ViewController::class, 'index'],
 
     '/error' => function () {
         return 'Could not find this page.';
